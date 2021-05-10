c9aa4b280332eb026df862b16773e7f75f827918
andy <00557106@email.ntou.edu.tw>
2021-05-10 22:37:11 +0800

    init

--- src/test/java/test/inejction/InejctionApplicationTests.java
+++ src/test/java/test/inejction/InejctionApplicationTests.java
@@ -1,13 +1,10 @@
 package test.inejction;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InejctionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
